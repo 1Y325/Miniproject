39904456cfdaded95a29b1b82e132581813f7a47
Yash Gupta <yashg3600@gmail.com>
2022-12-08 21:59:58 +0530

    first

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { images } from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Maintain loyal ones, and raise the overall profile of the establishment outside of the physical location. It's a crucial aspect of running a successful business. 
+</p>
+        {/* <button type="button" className="custom__button">Know More</button> */}
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Since our modest beginnings in 2005 with a little space in Toronto’s stylish Yorkville locale, ‘Organization Name’ ‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food.
+</p>
+        {/* <button type="button" className="custom__button">Know More</button> */}
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
